4b5ebda54051d71cfbadd2a479b8c8f1d529e243
Elmo <elmotabuzojr.ust@gmail.com>
2023-02-20 23:20:46 +0800

    react bootstrap setup using npm install react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h2>Welcome to proshop</h2>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
